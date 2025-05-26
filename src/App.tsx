6127364f84efb3733cdd0081c131d8d2dee27fb8
shayanabedi98 <shayanabedi@hotmail.com>
2025-05-26 17:17:49 -0400

    adjusted navbar and sidebar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
@@ -5,8 +6,13 @@ function App() {
   return (
     <div className="flex flex-col">
       <Navbar />
-      <div className="flex">
+      <div className="flex flex-1 sticky top-0">
         <Sidebar />
+        <div className="pl-64 w-full">
+          <Container>
+            <h1>Shayan</h1>
+          </Container>
+        </div>
       </div>
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,7 +12,7 @@ export default function Sidebar() {
   const [pathname, setPathname] = useState(window.location.pathname);
 
   return (
-    <nav className="min-h-[100lvh] w-64 border-y-0 flex flex-col gap-1 py-10 px-8 border-accent border-x">
+    <div className="w-64 fixed left-0 border-y-0 h-full flex flex-col gap-1 py-10 px-8 border-accent border-x">
       <h1 className="font-bold text-xl">Navigation</h1>
       <SidebarNavItem href="/#about" content="About" />
       <SidebarNavItem href="/#skills" content="Skills" />
@@ -21,6 +21,6 @@ export default function Sidebar() {
       <SidebarNavItem href="/#certifications" content="Certifications" />
       <SidebarNavItem href="/#soft" content="Soft Skills" />
       <SidebarNavItem href="/#contact" content="Contact" />
-    </nav>
+    </div>
   );
 }
